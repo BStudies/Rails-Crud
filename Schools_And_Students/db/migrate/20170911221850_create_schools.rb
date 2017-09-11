f6fff962fb1a9fb8120362bed08085bedcce76a4
brandon <digihew@gmail.com>
2017-09-11 18:21:36 -0400

    Update school migration

--- Schools_And_Students/db/migrate/20170911221850_create_schools.rb
+++ Schools_And_Students/db/migrate/20170911221850_create_schools.rb
@@ -1,7 +1,10 @@
 class CreateSchools < ActiveRecord::Migration[5.1]
   def change
     create_table :schools do |t|
-
+      t.has_many :students
+      t.string :name
+      t.string :location
+      
       t.timestamps
     end
   end
